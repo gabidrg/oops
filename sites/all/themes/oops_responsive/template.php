c2a4c4238b5834f3657adfc227ccc701681401a6
Gabriel Dragomir <gabriel.dragomir@webikon.com>
2013-11-25 12:46:10 +0200

    Oops utils and theme updates

--- sites/all/themes/oops_responsive/template.php
+++ sites/all/themes/oops_responsive/template.php
@@ -6,3 +6,21 @@
  */
 
 drupal_add_library('system', 'ui.tabs');
+
+/**
+ * Implementation of hook_preprocess_page().
+ */
+function oops_responsive_preprocess_page(&$variables) {
+  // Tab Tamer
+  $unwanted_tabs = array('user/register');
+  foreach ($variables['tabs'] as $group_key => $tab_group) {
+    if (is_array($tab_group)) {
+      foreach ($tab_group as $key => $tab) {
+        if (isset($tab['#link']['path']) && in_array($tab['#link']['path'], $unwanted_tabs)){
+          unset($variables['tabs'][$group_key][$key]);
+        }
+      }
+    }
+  }
+}
+
